f717df28fe69ea9227d1572524c32cb1984122a9
Wondi <wondi.tt@gmail.com>
2022-06-05 03:37:31 +0200

    key vault connect

---
+++ InventoryManagement/KeyVault/PrefixKeyVaultSecretManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.KeyVault
+{
+    public class PrefixKeyVaultSecretManager : IKeyVaultSecretManager
+    {
+        private readonly string _prefix;
+
+        public PrefixKeyVaultSecretManager(string prefix)
+        {
+            _prefix = $"{prefix}-";
+        }
+
+        public string GetKey(SecretBundle secret)
+        {
+            var ergebnis = secret.SecretIdentifier.Name.Substring(_prefix.Length)
+                .Replace("--", ConfigurationPath.KeyDelimiter);
+            return ergebnis;
+        }
+
+        public bool Load(SecretItem secret)
+        {
+            return secret.Identifier.Name.StartsWith(_prefix);
+        }
+    }
+}
--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using InventoryManagement.KeyVault;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,9 +20,36 @@ namespace InventoryManagement
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    var root = builder.Build();
+                    var vaultName = root["KeyVault:Vault"];
+                    builder.AddAzureKeyVault($"https://{vaultName}.vault.azure.net/",
+                        root["KeyVault:ClientId"], GetCertificate(root["KeyVault:Thumbprint"]),
+                        new PrefixKeyVaultSecretManager("InventoryMngment"));
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        private static X509Certificate2 GetCertificate(string thumbprint)
+        {
+            var store = new X509Store(StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certificateCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+
+                if (certificateCollection.Count == 0)
+                {
+                    throw new Exception("Certificate is not installed");
+                }
+                return certificateCollection[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
     }
 }
